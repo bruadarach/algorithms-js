17571dbde1af5e2c3fe58d71cd3daed7062065ab
Sujeong Ji <suji.sujeongji@gmail.com>
2022-11-28 03:40:27 +0900

    08. 베열 자르기

--- programmers/level-0/08-배열자르기/01-solution.js
+++ programmers/level-0/08-배열자르기/01-solution.js
@@ -1,8 +1,9 @@
 function solution(numbers, num1, num2) {
   //  return numbers.slice(num1, num2 + 1);
+
   // splice(start, deleteCount, item1, item2, itemN)
   return numbers.splice(num1, num2 - num1 + 1);
 }
 
-console.log(solution([1, 2, 3, 4, 5], 1, 3)); // [2,3,4]
+console.log(solution([1, 2, 3, 4, 5], 1, 3)); // [2,3,4] // numbers.splice(1, 3)
 console.log(solution([1, 3, 5], 1, 2)); // [3,5]
