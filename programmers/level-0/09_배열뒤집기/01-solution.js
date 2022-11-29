29cd93bbcc84351a5d86ee3e88c32fb5821bfec5
Sujeong Ji <suji.sujeongji@gmail.com>
2022-11-30 00:59:01 +0900

    09. 배열 뒤집기

---
+++ programmers/level-0/09_배열뒤집기/01-solution.js
@@ -0,0 +1,7 @@
+function solution(num_list) {
+  return num_list.reverse();
+}
+
+console.log(solution([1, 2, 3, 4, 5])); // [ 5, 4, 3, 2, 1 ]
+console.log(solution([1, 1, 1, 1, 1, 2])); // [ 2, 1, 1, 1, 1, 1 ]
+console.log(solution([1, 0, 1, 1, 1, 3, 5])); // [5, 3, 1, 1, 1, 0, 1]
