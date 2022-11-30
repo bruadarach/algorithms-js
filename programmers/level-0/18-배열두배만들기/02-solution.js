768b48e74cf4b32b497fa0a830d27246091957dd
Sujeong Ji <suji.sujeongji@gmail.com>
2022-12-01 03:04:28 +0900

    18. 배열 두 배 만들기

---
+++ programmers/level-0/18-배열두배만들기/02-solution.js
@@ -0,0 +1,6 @@
+function solution(numbers) {
+  return numbers.reduce((acc, cur) => [...acc, cur * 2], []);
+}
+
+console.log(solution([1, 2, 3, 4, 5])); // [2, 4, 6, 8, 10]
+console.log(solution([1, 2, 100, -99, 1, 2, 3])); // [2, 4, 200, -198, 2, 4, 6]
