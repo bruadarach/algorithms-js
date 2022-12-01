9b2683dc7aada3dcb6be1d8827aab1d7eb9e489d
Sujeong Ji <suji.sujeongji@gmail.com>
2022-12-02 03:45:41 +0900

    12. x만큼 간격이 있는 n개의 숫자

---
+++ programmers/level-1/12-x만큼간격이있는n개의숫자/01-solution.js
@@ -0,0 +1,8 @@
+function solution(x, n) {
+  return Array.from({ length: n }, (_, i) => (i + 1) * x);
+  //   return [...Array(n).keys()].map((ele, i) => (i + 1) * x);
+}
+
+console.log(solution(2, 5)); // [2,4,6,8,10]
+console.log(solution(4, 3)); // [4,8,12]
+console.log(solution(-4, 2)); // [-4, -8]
