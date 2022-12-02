54f6ec5295a2b90b7935fb0c228dcc1d5f96690f
Sujeong Ji <suji.sujeongji@gmail.com>
2022-12-02 19:44:36 +0900

    05. 이진 변환 반복하기

--- programmers/level-2/05-이진변환반복하기/02-solution.js
+++ programmers/level-2/05-이진변환반복하기/02-solution.js
@@ -4,6 +4,8 @@ function solution(s) {
   while (s.length !== 1) {
     const oldLen = s.length;
     s = s.replace(/0/g, "");
+    // s = s.replaceAll('0', '')
+    // s = [...s].filter((ele, i) => ele === "1");
     const newLen = s.length;
     s = newLen.toString(2);
 
