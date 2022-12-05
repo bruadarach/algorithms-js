fdc89b206c435dd100ecfb9147e4eaaec45ad9b6
Sujeong Ji <suji.sujeongji@gmail.com>
2022-12-05 11:15:42 +0900

    32. 문자열 내 마음대로 정렬하기

---
+++ programmers/level-1/32-문자열내마음대로정렬하기/01-solution.js
@@ -0,0 +1,8 @@
+function solution(strings, n) {
+  return strings.sort((a, b) =>
+    a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n])
+  );
+}
+
+console.log(solution(["sun", "bed", "car"], 1)); // ["car", "bed", "sun"]
+console.log(solution(["abce", "abcd", "cdx"], 2)); // ["abcd", "abce", "cdx"]
