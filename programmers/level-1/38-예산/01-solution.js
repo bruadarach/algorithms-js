f772179962e81a54b38b279ffe0a86eaa128125a
Sujeong Ji <suji.sujeongji@gmail.com>
2022-12-10 09:35:52 +0900

    38. 예산

---
+++ programmers/level-1/38-예산/01-solution.js
@@ -0,0 +1,18 @@
+function solution(d, budget) {
+  d.sort((a, b) => a - b);
+
+  let count = 0;
+  let sum = 0;
+  for (let i = 0; i < d.length; i++) {
+    count++;
+    sum += d[i];
+    if (sum > budget) {
+      count--;
+      break;
+    }
+  }
+  return count;
+}
+
+console.log(solution([1, 3, 2, 5, 4], 9)); // 3
+console.log(solution([2, 2, 3, 3], 10)); // 4
