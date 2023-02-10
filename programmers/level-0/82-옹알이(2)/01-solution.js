2b109bb60feb7208c72d2b99d61e45c22235c1a8
Sujeong Ji <suji.sujeongji@gmail.com>
2023-02-10 22:51:53 +0900

    Delete programmers/level-0/82-옹알이(2) directory

--- programmers/level-0/82-옹알이(2)/01-solution.js
+++
@@ -1,7 +0,0 @@
-function solution(babbling) {
-  var answer = 0;
-  return answer;
-}
-
-console.log(solution(["aya", "yee", "u", "maa", "wyeoo"])); // 1
-console.log(solution(["ayaye", "uuuma", "ye", "yemawoo", "ayaa"])); // 3
