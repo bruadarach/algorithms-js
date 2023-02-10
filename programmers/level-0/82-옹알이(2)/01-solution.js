24437827b710a0fb4204e05ef2b2c4cc01bd2c56
Sujeong Ji <suji.sujeongji@gmail.com>
2023-02-10 22:30:13 +0900

    82. 옹알이(2)

---
+++ programmers/level-0/82-옹알이(2)/01-solution.js
@@ -0,0 +1,7 @@
+function solution(babbling) {
+  var answer = 0;
+  return answer;
+}
+
+console.log(solution(["aya", "yee", "u", "maa", "wyeoo"])); // 1
+console.log(solution(["ayaye", "uuuma", "ye", "yemawoo", "ayaa"])); // 3
