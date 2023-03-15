7d203954e8f54174bdf06f004a57c73cd6483afc
Sujeong Ji <suji.sujeongji@gmail.com>
2023-03-16 00:46:28 +0900

    46. 소수 만들기

---
+++ programmers/level-1/46-소수만들기/01-solution.js
@@ -0,0 +1,25 @@
+function solution(nums) {
+  let answer = 0;
+  const len = nums.length;
+
+  const isPrime = (num) => {
+    if (num < 2) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  };
+
+  for (let i = 0; i < len - 2; i++) {
+    for (let j = i + 1; j < len - 1; j++) {
+      for (let k = j + 1; k < len; k++) {
+        if (isPrime(nums[i] + nums[j] + nums[k])) answer++;
+      }
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution([1, 2, 3, 4])); // 1
+console.log(solution([1, 2, 7, 6, 4])); // 4
