9433e6290ebfdc10ab78875c96d4de486d15252d
Sujeong Ji <suji.sujeongji@gmail.com>
2023-03-23 23:37:09 +0900

    feat: quick sort

---
+++ sorting-algorithms/05-quick-sort/01-solution.js
@@ -0,0 +1,22 @@
+// Big O
+// Worst case (when the array is already sorted): O(n^2)
+// Average case: O(n log n)
+
+function quickSort(arr) {
+  if (arr.length < 2) return arr;
+
+  let pivot = arr[arr.length - 1];
+  let left = [];
+  let right = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+const arr = [5, 3, 4, 1, 2];
+console.log(quickSort(arr)); // [1, 2, 3, 4, 5]
