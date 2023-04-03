1f199bc5d5b7258a590acb090c514991e7056a9f
Sujeong Ji <suji.sujeongji@gmail.com>
2023-04-03 22:54:15 +0900

    51. 추억 점수

---
+++ programmers/level-1/51-추억점수/01-solution.js
@@ -0,0 +1,44 @@
+function solution(name, yearning, photo) {
+  return photo.map((el) =>
+    el.reduce(
+      (acc, cur) =>
+        name.includes(cur) ? acc + yearning[name.indexOf(cur)] : acc,
+      0
+    )
+  );
+}
+
+console.log(
+  solution(
+    ["may", "kein", "kain", "radi"],
+    [5, 10, 1, 3],
+    [
+      ["may", "kein", "kain", "radi"],
+      ["may", "kein", "brin", "deny"],
+      ["kon", "kain", "may", "coni"],
+    ]
+  )
+);
+// [19, 15, 6]
+
+console.log(
+  solution(
+    ["kali", "mari", "don"],
+    [11, 1, 55],
+    [
+      ["kali", "mari", "don"],
+      ["pony", "tom", "teddy"],
+      ["con", "mona", "don"],
+    ]
+  )
+);
+// [67, 0, 55]
+
+console.log(
+  solution(
+    ["may", "kein", "kain", "radi"],
+    [5, 10, 1, 3],
+    [["may"], ["kein", "deny", "may"], ["kon", "coni"]]
+  )
+);
+// [5, 15, 0]
