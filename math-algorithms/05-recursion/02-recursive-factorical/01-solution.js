c15d67c3aecce373aac7a570ad010b9dda2c1151
Sujeong Ji <suji.sujeongji@gmail.com>
2023-05-03 23:53:46 +0900

    feat: recursive factorial of a number

---
+++ math-algorithms/05-recursion/02-recursive-factorical/01-solution.js
@@ -0,0 +1,15 @@
+// Big-O : O(n)
+
+function recursiveFactorial(n) {
+  if (n === 0) {
+    return 1;
+  }
+  return n * recursiveFactorial(n - 1);
+}
+
+console.log(recursiveFactorial(5)); // 120
+console.log(recursiveFactorial(4)); // 24
+console.log(recursiveFactorial(3)); // 6
+console.log(recursiveFactorial(2)); // 2
+console.log(recursiveFactorial(1)); // 1
+console.log(recursiveFactorial(0)); // 1
--- math-algorithms/05-recursion/02-recursive-factorical/README.md
+++ math-algorithms/05-recursion/02-recursive-factorical/README.md
@@ -28,3 +28,7 @@ The factorial of a non-negative integer 'n' is the product of all positive integ
   - 0! = 1
 
 => n! = n \* (n-1)!
+
+## Big O
+
+- Time Complexity : O(n)
