457032839a8d63db83019d812c09acf223d0df8c
Sujeong Ji <suji.sujeongji@gmail.com>
2023-06-08 17:16:33 +0900

    142. Linked List Cycle II

--- leetcode/142-linked-list-cycle-||/01-solution.js
+++ leetcode/142-linked-list-cycle-||/01-solution.js
@@ -11,15 +11,21 @@
  * @return {ListNode}
  */
 var detectCycle = function (head) {
+  // 1. find a loop (cycle) using two pointers
   let slow = head;
   let fast = head;
   while (fast && fast.next && fast.next.next) {
     slow = slow.next;
+    // move by 2 steps
     fast = fast.next.next;
+
+    // if slow and fast pointers meet, then there is a loop
+    // 2. find the entrance of the loop (cycle)
     if (slow === fast) {
       slow = head;
       while (slow !== fast) {
         slow = slow.next;
+        //
         fast = fast.next;
       }
       return slow;
