d712f414e03d8db17b98fa26ccef2dff9017992a
Sujeong Ji <suji.sujeongji@gmail.com>
2023-06-13 23:59:28 +0900

    fix: remove a file

--- coplit/daily1/5.js
+++
@@ -1,9 +0,0 @@
-function firstReverse(str) {
-  return str.split("").reverse().join("");
-}
-
-let output = firstReverse("codestates");
-console.log(output); // "setatsedoc"
-
-output = firstReverse("I love codestates");
-console.log(output); // "setatsedoc evol I"
