193dfd639943e4473e6cf88c159c6d7ef0ca70a3
Sujeong Ji <suji.sujeongji@gmail.com>
2023-06-13 23:42:17 +0900

    feat: get

---
+++ ds-algo/02-doubly-linked-lists/06-get/01-get.js
@@ -0,0 +1,118 @@
+class Node {
+  constructor(value) {
+    this.val = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLikedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined; // if this.tail === null // if this.length === 0
+
+    let temp = this.tail;
+    this.tail = this.tail.prev;
+    this.tail.next = null;
+    temp.prev = null;
+    this.length--;
+    // Edge case: if this.length === 0
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return temp;
+  }
+
+  unshift(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  shift() {
+    if (this.length === 0) return undefined;
+
+    let temp = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      let temp = this.head;
+      this.head = this.head.next;
+      this.head.prev = null; // problematic line
+      temp.next = null;
+    }
+    this.length--;
+    return temp;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return undefined;
+
+    let temp = this.head;
+
+    if (index <= this.length / 2) {
+      for (let i = 0; i < index; i++) {
+        temp = temp.next;
+      }
+    } else {
+      temp = this.tail;
+      for (let i = this.length - 1; i > index; i--) {
+        temp = temp.prev;
+      }
+    }
+    return temp;
+  }
+}
+
+let myDoublyLinkedList = new DoublyLikedList(1);
+myDoublyLinkedList.push(2);
+myDoublyLinkedList.push(3);
+myDoublyLinkedList.push(4);
+myDoublyLinkedList.push(5);
+console.log(myDoublyLinkedList.get(5)); // undefined
+console.log(myDoublyLinkedList.get(2));
+{
+  /* <ref *2> Node {
+  val: 3,
+  next: <ref *1> Node {
+    val: 4,
+    next: Node { val: 5, next: null, prev: [Circular *1] },
+    prev: [Circular *2]
+  },
+  prev: <ref *3> Node {
+    val: 2,
+    next: [Circular *2],
+    prev: Node { val: 1, next: [Circular *3], prev: null }
+  }
+} */
+}
