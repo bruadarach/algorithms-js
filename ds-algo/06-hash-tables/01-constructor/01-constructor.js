6974c829b2a43c6cd473c9b54982d0b37889e1a6
Sujeong Ji <suji.sujeongji@gmail.com>
2023-06-18 07:15:50 +0900

    feat: constructor

--- ds-algo/06-hash-tables/01-constructor/01-constructor.js
+++ ds-algo/06-hash-tables/01-constructor/01-constructor.js
@@ -8,7 +8,7 @@ class HashTable {
   // '_' means this is a method that should only be called by other methods
   // this method is to generate a random prime number
   _hash(key) {
-    let hahs = 0;
+    let hash = 0;
     for (let i = 0; i < key.length; i++) {
       // '23' is a prime number, and it's helpful in spreading out the keys more uniformly
       // this.dataMap.length always ranges from 0 to 6
